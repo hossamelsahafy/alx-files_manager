2e84149fcebb72f302b98f38612bf837a7040956
hossamelsahafy <hossamelsa7afy54@gmail.com>
2024-08-06 14:06:17 +0300

    Files

--- controllers/UsersController.js
+++ controllers/UsersController.js
@@ -54,6 +54,31 @@ class UsersController {
 
     return res.status(200).json({ id: user._id.toString(), email: user.email });
   }
+  static async postUser(req, res) {
+    const { email, password } = req.body;
+  
+    if (!email || !password) {
+      return res.status(400).json({ error: 'Missing email or password' });
+    }
+  
+    const existingUser = await dbClient.db.collection('users').findOne({ email });
+  
+    if (existingUser) {
+      return res.status(400).json({ error: 'User already exists' });
+    }
+  
+    const newUser = {
+      email,
+      password,
+    };
+  
+    const result = await dbClient.db.collection('users').insertOne(newUser);
+    const userId = result.insertedId;
+  
+    await userQueue.add({ userId });
+  
+    return res.status(201).json({ id: userId, email });
+  }
 }
 
 export default UsersController;
